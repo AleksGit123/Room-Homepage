11c4f3a189e877c2ecdf36845d71e2dcfaad6d29
AleksGit123 <aleksichokheli03@gmail.com>
2025-08-22 16:39:25 +0400

    final project

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+let {src,dest,series,watch} = require('gulp');
+let sass = require('gulp-sass')(require ('sass'));
+
+function buildStyles(){
+    return src("scss/**/*.scss")
+    .pipe(sass())
+    .pipe(dest("./css"));
+}
+
+function watchTask (){
+    watch('scss/**/*.scss', buildStyles);
+}
+
+exports.default = series(buildStyles,watchTask);
---
+++ script.js
@@ -0,0 +1,38 @@
+// burger variables
+
+let burgerMenu = document.querySelector(".burger__menu");
+let burgerMenuList = document.querySelector(".burger__menu__list");
+let closeIcon = document.querySelector(".close__icon");
+
+// scroll images
+let scrollImages = ["./images/desktop-image-hero-1.jpg","./images/desktop-image-hero-2.jpg","./images/desktop-image-hero-3.jpg"];
+let arrowLeft = document.querySelector(".arrow__left");
+let arrowRight = document.querySelector(".arrow__right");
+let imageDiv = document.querySelector(".image__div1")
+let index = 0;
+
+burgerMenu.addEventListener("click",()=>{
+    burgerMenuList.classList.add("active");
+})
+
+closeIcon.addEventListener("click",()=>{
+    burgerMenuList.classList.remove("active");
+})
+
+arrowLeft.addEventListener("click",()=>{
+    index--;
+    if(index<0){
+        index = scrollImages.length-1;
+    }
+
+    console.log(index)
+})
+
+arrowRight.addEventListener("click",()=>{
+    index++;
+    if(index>scrollImages.length-1){
+        index = 0;
+    }
+    imageDiv.style.backgroundImage = `url(${scrollImages[index]})`;
+    console.log(index)
+})
